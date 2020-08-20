9daaeed49a2734391b0623335dfb35140863aad5
brt <brt@Pro.local>
2020-08-20 10:09:01 +0200

    navbar added

---
+++ src/components/FirstStep.vue
@@ -0,0 +1,14 @@
+<template>
+    <label for="service-type">Service type</label>
+    <input type="radio" id="service" name="service" v-model="service"/>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            service: '',
+        }
+    }
+}
+</script>
